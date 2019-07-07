f83bf45e303c31b522c660d5847dd5497e20e5cf
tomotaka <tomobrosk@gmail.com>
2019-07-07 14:33:59 +0900

    add vuetify

---
+++ src/components/Speaker.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>hello</p>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    export default class Speaker extends Vue{
+
+    }
+</script>
+
+
+<style scoped lang="less">
+</style>
+
+
