ae6ad25a0e2b8afddb5e9b3db893f07e668f472d
Tomotaka Kato <tomo@Tomotakas-MacBook-Pro.local>
2019-07-07 13:56:59 +0900

    server.jsのコンソール表示を追加

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const port = process.env.PORT ||  8080;
 const app = express();
 app.use(express.static(__dirname + "/dist/"));
 
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
-
 app.listen(port);
 
+console.log("Server is up!!")
