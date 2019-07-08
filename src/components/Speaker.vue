18e604f29eb9872afd73d5ba27ffa9119c08f926
Tomotaka Kato <kato.tomotaka@smileforce.jp>
2019-07-08 16:38:33 +0900

    modify

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -1,63 +1,60 @@
 <template>
-    <v-app id="inspire"> <v-container>
-            <v-layout row wrap>
-                <v-flex class="text-xs-center" xs12 md5>
-                    <v-text-field
-                        clearable
-                        v-model="message"
-                        placeholder="発話させたい文言を入力してください"
+    <v-layout row wrap>
+        <v-flex class="text-xs-center" xs12 md5>
+            <v-text-field
+                clearable
+                v-model="message"
+                placeholder="発話させたい文言を入力してください"
+                >
+            </v-text-field>
+                <v-btn fab color=primary @click="speak">
+                    <v-icon dark>volume_up</v-icon>
+                </v-btn>
+        </v-flex>
+
+        <v-flex xs12 md5 >
+            <v-slider
+                label="音量"
+                v-model="volume"
+                min=0
+                max=1
+                step=0.1
+                >
+            </v-slider> 
+                <v-slider
+                    label="速度"
+                    v-model="rate"
+                    min=0.1
+                    max=3.5
+                    step=0.1
+                    :value="rate"
+                    >
+                </v-slider> 
+                    <v-slider
+                        label="音程"
+                        v-model="pitch"
+                        min=0
+                        max=2
+                        step=0.1
+                        :value="pitch"
                         >
-                    </v-text-field>
-                    <v-btn fab color=primary @click="speak">
-                        <v-icon dark>volume_up</v-icon>
-                    </v-btn>
-                </v-flex>
+                    </v-slider> 
+        </v-flex>
 
-                <v-flex xs12 md5 >
-                       <v-slider
-                           label="音量"
-                           v-model="volume"
-                           min=0
-                           max=1
-                           step=0.1
-                       >
-                       </v-slider> 
-                       <v-slider
-                           label="速度"
-                           v-model="rate"
-                           min=0.1
-                           max=3.5
-                           step=0.1
-                           :value="rate"
-                       >
-                       </v-slider> 
-                       <v-slider
-                           label="音程"
-                           v-model="pitch"
-                           min=0
-                           max=2
-                           step=0.1
-                           :value="pitch"
-                       >
-                       </v-slider> 
-                </v-flex>
+            <v-flex xs12 >
+                <v-card>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">エラーメッセージ</h3>
+                            <div> {{ errorMsg }} </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
 
-                <v-flex xs12 >
-                    <v-card>
-                        <v-card-title primary-title>
-                            <div>
-                                <h3 class="headline mb-0">エラーメッセージ</h3>
-                                <div> {{ errorMsg }} </div>
-                            </div>
-                        </v-card-title>
-                    </v-card>
-                </v-flex>
 
-                    
 
-            </v-layout>
-        </v-container>
-    </v-app>
+    </v-layout>
 </template>
 
 <script lang="ts">
@@ -86,7 +83,7 @@
                 this.msg.rate = this.rate;
                 this.msg.pitch = this.pitch
                 this.msg.lang = 'ja-JP'
-                this.message.length > 0
+                this.message
                     ? this.msg.text = this.message
                     : this.msg.text = "サンプルメッセージです"
 
@@ -103,9 +100,4 @@
         }
     }
 </script>
-
-
-<style scoped lang="less">
-</style>
-
 
